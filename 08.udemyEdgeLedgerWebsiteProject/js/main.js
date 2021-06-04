21b555004c214b35d2fcb3c77f7888ac812e233f
Yavor Petkov <hermsues@gmail.com>
2021-06-04 17:27:40 +0300

    the Udemy Edge Ledger website project

---
+++ 08.udemyEdgeLedgerWebsiteProject/js/main.js
@@ -0,0 +1,33 @@
+function initMap() {
+	const loc = { lat: 42.361145, lng: -71.057083 };
+	const map = new google.maps.Map(document.querySelector('.map'), {
+		center: loc,
+		zoom: 14
+	});
+	const marker = new google.maps.marker({ position: loc, map: map });
+}
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+	if (this.hash !== '') {
+		event.preventDefault();
+
+		const hash = this.hash;
+
+		$('html, body').animate(
+			{
+				scrollTop: $(hash).offset().top - 100
+			},
+			800
+		);
+	}
+});
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+	if (window.scrollY > 150) {
+		document.querySelector('#navbar').style.opacity = 0.9;
+	} else {
+		document.querySelector('#navbar').style.opacity = 1;
+	}
+});
